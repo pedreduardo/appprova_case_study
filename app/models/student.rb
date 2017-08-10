dae069a3eacce5533f20c6346d3b20374be0ca27
Pedro Eduardo <pedroeduardo.contato@gmail.com>
2017-08-10 11:38:48 -0300

    fix searches' behavior and layout

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,6 @@
 class Student < ApplicationRecord
   belongs_to :member
-  scope :filter, ->(arg){ where("name LIKE ? OR enem_number LIKE ?", "%#{arg}%", "%#{arg}%").order('name') }
+  scope :filter, ->(search){ where("name LIKE ? OR enem_number LIKE ?", "%#{search}%", "%#{search}%").order('name') }
+  scope :mine, ->(member_id){ where(member_id: member_id.id).order('name') }
+  scope :not_mine, ->(member_id){ where.not(member_id: member_id.id).order('name') }
 end
