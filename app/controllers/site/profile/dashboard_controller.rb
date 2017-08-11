06cd332a0f8bce65c9435f89279f4ec72b347bff
Renan Garcia <email@renangarcia.me>
2017-08-11 02:23:13 -0300

    Fix graphics

--- app/controllers/site/profile/dashboard_controller.rb
+++ app/controllers/site/profile/dashboard_controller.rb
@@ -1,15 +1,11 @@
 class Site::Profile::DashboardController < Site::ProfileController
-
   def index
     @students = current_member.students.limit(500)
-    members = Member
-              .joins(:students)
-              .select('members.id ,members.name_school, count(students.id) as student_count')
-              .group('members.id')
+    members = Member.average_students
     amount = 0
     members.each do |member|
       amount += member['student_count']
     end
-    @average_of_students = amount / members.size
+    @average_of_students = members.size.zero? ? 0 : (amount / members.size)
   end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,6 +1,13 @@
 class Member < ApplicationRecord
   validates_presence_of :first_name, :last_name, :name_school, :cnpj_school, :cell_phone
   has_many :students
+
+  scope :average_students, -> {
+    where.not(id: 1)
+         .joins(:students)
+         .select('members.id ,members.name_school, count(students.id) as student_count')
+         .group('members.id')
+  }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
